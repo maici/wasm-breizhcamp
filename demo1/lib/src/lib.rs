de3011b506cf0a8e475dfe02eb397e77da1ecdf4
Mahdi AICI <aici.mahdi@gmail.com>
2022-07-01 09:35:03 +0000

    cleanup

--- demo1/lib/src/lib.rs
+++ demo1/lib/src/lib.rs
@@ -1,13 +1,5 @@
-mod utils;
-
 use pulldown_cmark::{html, Parser};
 use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn parse_markdown(makrdown: String) -> String {
